03cbbe278eee4b9ba4cd97a2a0767e246bd270a9
Rafhaan Shah <RafhaanShah@users.noreply.github.com>
2021-01-13 11:47:55 +0000

    Update setup script

--- setup.sh
+++ setup.sh
@@ -3,22 +3,45 @@
 # exit on any error
 set -e
 
-# repo folder
+# dotfile repo folder
 DOTFILE_DIR="${HOME}/Dotfiles"
-# list of files
-DOTFILES=(
-    .hushlogin
+
+# folder to backup existing dotfiles
+BACKUP_DIR="${DOTFILE_DIR}/old"
+
+# list of files to symlink to repo
+SYM_FILES=(
     .bash_profile
     .bashrc
     .zshrc
+)
+    
+# list of files to copy so they can be modified as needed
+CP_FILES=(
+    .hushlogin
+    .gitconfig
     .nanorc
 )
+    
+run_setup() {
+    [ ! -d "${DOTFILE_DIR}" ] && echo "Inavlid Dotfile folder" && exit 1
+    mkdir -p "${DOTFILE_DIR}/old"
+    copy_dotfiles
+    symlink_dotfiles
+}
 
-# add .old to existing files and symlink repo files to home directory
-add_symlinks() {
+copy_dotfiles() {
     for FILE in "${DOTFILES[@]}"; do
         echo "${FILE}"
-        [ -f "${HOME}/${FILE}" ] && mv "${HOME}/${FILE}" "${HOME}/${FILE}.old"
+        [ -f "${HOME}/${FILE}" ] && mv "${HOME}/${FILE}" "${BACKUP_DIR}/${FILE}.old"
+        cp "${CP_FILES}/${FILE}" "${HOME}/${FILE}"
+    done
+}
+
+symlink_dotfiles() {
+    for FILE in "${SYM_FILES[@]}"; do
+        echo "${FILE}"
+        [ -f "${HOME}/${FILE}" ] && mv "${HOME}/${FILE}" "${BACKUP_DIR}/${FILE}.old"
         ln -s "${DOTFILE_DIR}/${FILE}" "${HOME}/${FILE}"
     done
 }
@@ -30,5 +53,5 @@ read -p "Continue (y/n)? " CONT
 if [ "$CONT" != "y" ]; then
     exit 1
 else
-    add_symlinks && echo "Done"
+    run_setup && echo "Done"
 fi
