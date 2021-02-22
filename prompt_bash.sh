2b901eac1061f389397acbbd9370e14e21c99016
Rafhaan Shah <RafhaanShah@users.noreply.github.com>
2021-02-22 15:49:20 +0000

    Add more formatting

--- prompt_bash.sh
+++ prompt_bash.sh
@@ -6,9 +6,9 @@
 _prompt_command() {
     local EXIT="$?"
     PS1="\[\e]0;\u@\h: \w\a\]" # set terminal title
-    PS1+="\n${_PROMPT_STR}\n" # user, host, dir, git
-    _prompt_dir_changed && ls # auto ls if dir changed
-    
+    PS1+="\n${_PROMPT_STR}\n"  # user, host, dir, git
+    _prompt_dir_changed && ls  # auto ls if dir changed
+
     if [ "${EXIT}" -eq 0 ]; then
         PS1+="${_PROMPT_CH_OK}"
     else
@@ -19,41 +19,41 @@ _prompt_command() {
 PROMPT_COMMAND=_prompt_command
 
 if _colour_true; then
-    _PROMPT_COL_USER="\[\e[1;38;2;166;226;46m\]" # green
-    _PROMPT_COL_HOST="\[\e[1;38;2;253;151;31m\]" # orange
-    _PROMPT_COL_DIR="\[\e[1;38;2;102;217;239m\]" # cyan
-    _PROMPT_COL_GIT="\[\e[1;38;2;249;38;114m\]" # pink
-    _PROMPT_COL_OK="\[\e[1;38;2;166;226;46m\]" # green
-    _PROMPT_COL_ERR="\[\e[1;38;2;248;53;53m\]" # red
+    _PROMPT_COL_USER="\[\e[1;38;2;166;226;46m\]"  # green
+    _PROMPT_COL_HOST="\[\e[1;38;2;253;151;31m\]"  # orange
+    _PROMPT_COL_DIR="\[\e[1;38;2;102;217;239m\]"  # cyan
+    _PROMPT_COL_GIT="\[\e[1;38;2;249;38;114m\]"   # pink
+    _PROMPT_COL_OK="\[\e[1;38;2;166;226;46m\]"    # green
+    _PROMPT_COL_ERR="\[\e[1;38;2;248;53;53m\]"    # red
     _PROMPT_COL_NORM="\[\e[1;38;2;248;248;241m\]" # white
 elif _colour_256; then
     _PROMPT_COL_USER="\[\e[1;38;5;112m\]" # green
     _PROMPT_COL_HOST="\[\e[1;38;5;208m\]" # orange
-    _PROMPT_COL_DIR="\[\e[1;38;5;81m\]" # cyan
-    _PROMPT_COL_GIT="\[\e[1;38;5;197m\]" # pink
-    _PROMPT_COL_OK="\[\e[1;38;5;112m\]" # green
-    _PROMPT_COL_ERR="\[\e[1;38;5;196m\]" # red
+    _PROMPT_COL_DIR="\[\e[1;38;5;81m\]"   # cyan
+    _PROMPT_COL_GIT="\[\e[1;38;5;197m\]"  # pink
+    _PROMPT_COL_OK="\[\e[1;38;5;112m\]"   # green
+    _PROMPT_COL_ERR="\[\e[1;38;5;196m\]"  # red
     _PROMPT_COL_NORM="\[\e[1;38;5;255m\]" # white
 else
     _PROMPT_COL_USER="\[\e[1;92m\]" # green
     _PROMPT_COL_HOST="\[\e[1;93m\]" # yellow
-    _PROMPT_COL_DIR="\[\e[1;96m\]" # cyan
-    _PROMPT_COL_GIT="\[\e[1;91m\]" # red
-    _PROMPT_COL_OK="\[\e[1;92m\]" # green
-    _PROMPT_COL_ERR="\[\e[1;91m\]" # red
+    _PROMPT_COL_DIR="\[\e[1;96m\]"  # cyan
+    _PROMPT_COL_GIT="\[\e[1;91m\]"  # red
+    _PROMPT_COL_OK="\[\e[1;92m\]"   # green
+    _PROMPT_COL_ERR="\[\e[1;91m\]"  # red
     _PROMPT_COL_NORM="\[\e[1;97m\]" # white
 fi
 
 _PROMPT_COL_RESET="\[\e[0m\]" # reset colour
-_PROMPT_ARROW=$'\u2192' # git arrow
-_PROMPT_CHAR=$'\u276F' # prompt character
+_PROMPT_ARROW=$'\u2192'       # git arrow
+_PROMPT_CHAR=$'\u276F'        # prompt character
 
-_PROMPT_STR="${_PROMPT_COL_USER}\u " # user
-_PROMPT_STR+="${_PROMPT_COL_NORM}@ ${_PROMPT_COL_HOST}\h " # host
-_PROMPT_STR+="${_PROMPT_COL_NORM}: ${_PROMPT_COL_DIR}\w " # directory
+_PROMPT_STR="${_PROMPT_COL_USER}\u "                                                      # user
+_PROMPT_STR+="${_PROMPT_COL_NORM}@ ${_PROMPT_COL_HOST}\h "                                # host
+_PROMPT_STR+="${_PROMPT_COL_NORM}: ${_PROMPT_COL_DIR}\w "                                 # directory
 _PROMPT_STR+="\$(_prompt_git \"${_PROMPT_COL_NORM}${_PROMPT_ARROW} ${_PROMPT_COL_GIT}\")" # git
 
-_PROMPT_CH_OK="${_PROMPT_COL_OK}${_PROMPT_CHAR} ${_PROMPT_COL_RESET}" # success prompt
+_PROMPT_CH_OK="${_PROMPT_COL_OK}${_PROMPT_CHAR} ${_PROMPT_COL_RESET}"   # success prompt
 _PROMPT_CH_ERR="${_PROMPT_COL_ERR}${_PROMPT_CHAR} ${_PROMPT_COL_RESET}" # error prompt
 
 unset _PROMPT_COL_USER _PROMPT_COL_HOST _PROMPT_COL_DIR
--- prompt_zsh.sh
+++ prompt_zsh.sh
@@ -5,11 +5,12 @@
 
 # runs after each command, before PROMPT is printed
 _precmd_prompt() {
-    local EXIT="$?"; local _NEWLINE=$'\n' # new line
-    PROMPT=$'%{\e]0;%n@%m: %3~\a%}' # set terminal title
+    local EXIT="$?"
+    local _NEWLINE=$'\n'                           # new line
+    PROMPT=$'%{\e]0;%n@%m: %3~\a%}'                # set terminal title
     PROMPT+="${_NEWLINE}${_PROMPT_STR}${_NEWLINE}" # user, host, dir, git
-    _prompt_dir_changed && ls # auto ls if dir changed
-    
+    _prompt_dir_changed && ls                      # auto ls if dir changed
+
     if [ "${EXIT}" -eq 0 ]; then
         PROMPT+="${_PROMPT_CH_OK}"
     else
@@ -17,67 +18,71 @@ _precmd_prompt() {
     fi
 }
 
-precmd_functions+=( _precmd_prompt )
-
-typeset -gA ZSH_HIGHLIGHT_STYLES # define array for highlighting styles
+precmd_functions+=(_precmd_prompt)
 
 if _colour_true; then
-    _PROMPT_COL_USER=$'%{\e[1;38;2;166;226;46m%}' # green
-    _PROMPT_COL_HOST=$'%{\e[1;38;2;253;151;31m%}' # orange
-    _PROMPT_COL_DIR=$'%{\e[1;38;2;102;217;239m%}' # cyan
-    _PROMPT_COL_GIT=$'%{\e[1;38;2;249;38;114m%}' # pink
-    _PROMPT_COL_OK=$'%{\e[1;38;2;166;226;46m%}' # green
-    _PROMPT_COL_ERR=$'%{\e[1;38;2;248;53;53m%}' # red
+    _PROMPT_COL_USER=$'%{\e[1;38;2;166;226;46m%}'  # green
+    _PROMPT_COL_HOST=$'%{\e[1;38;2;253;151;31m%}'  # orange
+    _PROMPT_COL_DIR=$'%{\e[1;38;2;102;217;239m%}'  # cyan
+    _PROMPT_COL_GIT=$'%{\e[1;38;2;249;38;114m%}'   # pink
+    _PROMPT_COL_OK=$'%{\e[1;38;2;166;226;46m%}'    # green
+    _PROMPT_COL_ERR=$'%{\e[1;38;2;248;53;53m%}'    # red
     _PROMPT_COL_NORM=$'%{\e[1;38;2;248;248;241m%}' # white
-    
+
     ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE='fg=#9e9e9e' # grey
-    ZSH_HIGHLIGHT_STYLES[arg0]='fg=#a6e22e,bold' # green
-    ZSH_HIGHLIGHT_STYLES[path]='fg=#66d9ef' # blue
-    ZSH_HIGHLIGHT_STYLES[dollar-quoted-argument]='fg=#ffd75f' # yellow
-    ZSH_HIGHLIGHT_STYLES[double-quoted-argument]='fg=#ffd75f' # yellow
-    ZSH_HIGHLIGHT_STYLES[single-quoted-argument]='fg=#ffd75f' # yellow
+    typeset -gA ZSH_HIGHLIGHT_STYLES=(
+        ['arg0']='fg=#a6e22e,bold'              # green
+        ['path']='fg=#66d9ef'                   # blue
+        ['dollar-quoted-argument']='fg=#ffd75f' # yellow
+        ['double-quoted-argument']='fg=#ffd75f' # yellow
+        ['single-quoted-argument']='fg=#ffd75f' # yellow
+    )
 elif _colour_256; then
     _PROMPT_COL_USER=$'%{\e[1;38;5;112m%}' # green
     _PROMPT_COL_HOST=$'%{\e[1;38;5;208m%}' # orange
-    _PROMPT_COL_DIR=$'%{\e[1;38;5;81m%}' # cyan
-    _PROMPT_COL_GIT=$'%{\e[1;38;5;197m%}' # pink
-    _PROMPT_COL_OK=$'%{\e[1;38;5;112m%}' # green
-    _PROMPT_COL_ERR=$'%{\e[1;38;5;196m%}' # red
+    _PROMPT_COL_DIR=$'%{\e[1;38;5;81m%}'   # cyan
+    _PROMPT_COL_GIT=$'%{\e[1;38;5;197m%}'  # pink
+    _PROMPT_COL_OK=$'%{\e[1;38;5;112m%}'   # green
+    _PROMPT_COL_ERR=$'%{\e[1;38;5;196m%}'  # red
     _PROMPT_COL_NORM=$'%{\e[1;38;5;255m%}' # white
-    
+
     ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE='fg=247' # grey
-    ZSH_HIGHLIGHT_STYLES[arg0]='fg=112,bold' # green
-    ZSH_HIGHLIGHT_STYLES[path]='fg=81' # cyan
-    ZSH_HIGHLIGHT_STYLES[dollar-quoted-argument]='fg=221' # yellow
-    ZSH_HIGHLIGHT_STYLES[double-quoted-argument]='fg=221' # yellow
-    ZSH_HIGHLIGHT_STYLES[single-quoted-argument]='fg=221' # yellow
+    typeset -gA ZSH_HIGHLIGHT_STYLES=(
+        ['arg0']='fg=112,bold'              # green
+        ['path']='fg=81'                    # cyan
+        ['dollar-quoted-argument']='fg=221' # yellow
+        ['double-quoted-argument']='fg=221' # yellow
+        ['single-quoted-argument']='fg=221' # yellow
+    )
 else
     _PROMPT_COL_USER=$'%{\e[1;92m%}' # green
     _PROMPT_COL_HOST=$'%{\e[1;93m%}' # yellow
-    _PROMPT_COL_DIR=$'%{\e[1;96m%}' # cyan
-    _PROMPT_COL_GIT=$'%{\e[1;91m%}' # red
-    _PROMPT_COL_OK=$'%{\e[1;92m%}' # green
-    _PROMPT_COL_ERR=$'%{\e[1;91m%}' # red
+    _PROMPT_COL_DIR=$'%{\e[1;96m%}'  # cyan
+    _PROMPT_COL_GIT=$'%{\e[1;91m%}'  # red
+    _PROMPT_COL_OK=$'%{\e[1;92m%}'   # green
+    _PROMPT_COL_ERR=$'%{\e[1;91m%}'  # red
     _PROMPT_COL_NORM=$'%{\e[1;97m%}' # white
-    
+
     ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE='fg=8' # grey
-    ZSH_HIGHLIGHT_STYLES[arg0]='fg=10,bold' # green
-    ZSH_HIGHLIGHT_STYLES[path]='fg=14' # cyan
-    ZSH_HIGHLIGHT_STYLES[dollar-quoted-argument]='fg=11' # yellow
-    ZSH_HIGHLIGHT_STYLES[double-quoted-argument]='fg=11' # yellow
-    ZSH_HIGHLIGHT_STYLES[single-quoted-argument]='fg=11' # yellow
+    typeset -gA ZSH_HIGHLIGHT_STYLES=(
+        ['arg0']='fg=10,bold'              # green
+        ['path']='fg=14'                   # cyan
+        ['dollar-quoted-argument']='fg=11' # yellow
+        ['double-quoted-argument']='fg=11' # yellow
+        ['single-quoted-argument']='fg=11' # yellow
+    )
 fi
 
 _PROMPT_COL_RESET=$'%{\e[0m%}' # reset colour
-_PROMPT_ARROW=$'\u2192' # git arrow
-_PROMPT_CHAR=$'\u276F' # prompt character
+_PROMPT_ARROW=$'\u2192'        # git arrow
+_PROMPT_CHAR=$'\u276F'         # prompt character
 
-_PROMPT_STR="${_PROMPT_COL_USER}%n " # user
-_PROMPT_STR+="${_PROMPT_COL_NORM}@ ${_PROMPT_COL_HOST}%m " # host
-_PROMPT_STR+="${_PROMPT_COL_NORM}: ${_PROMPT_COL_DIR}%3~ " # directory
+_PROMPT_STR="${_PROMPT_COL_USER}%n "                                                    # user
+_PROMPT_STR+="${_PROMPT_COL_NORM}@ ${_PROMPT_COL_HOST}%m "                              # host
+_PROMPT_STR+="${_PROMPT_COL_NORM}: ${_PROMPT_COL_DIR}%3~ "                              # directory
 _PROMPT_STR+="\$(_prompt_git '${_PROMPT_COL_NORM}${_PROMPT_ARROW} ${_PROMPT_COL_GIT}')" # git
 
-_PROMPT_CH_OK="${_PROMPT_COL_OK}${_PROMPT_CHAR} ${_PROMPT_COL_RESET}" # success prompt
+_PROMPT_CH_OK="${_PROMPT_COL_OK}${_PROMPT_CHAR} ${_PROMPT_COL_RESET}"   # success prompt
 _PROMPT_CH_ERR="${_PROMPT_COL_ERR}${_PROMPT_CHAR} ${_PROMPT_COL_RESET}" # error prompt
 
 unset _PROMPT_COL_USER _PROMPT_COL_HOST _PROMPT_COL_DIR
