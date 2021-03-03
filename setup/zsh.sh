94d0ee29f516ade070744da0aee548e101656626
Rafhaan Shah <RafhaanShah@users.noreply.github.com>
2021-03-03 17:49:31 +0000

    Update zsh setup

--- setup/zsh.sh
+++ setup/zsh.sh
@@ -6,10 +6,8 @@ set -eo pipefail
 
 # setup zinit https://github.com/zdharma/zinit
 if [[ ! -f "${HOME}/.zinit/bin/zinit.zsh" ]]; then
-    print -P "%F{33}▓▒░ %F{220}Installing %F{33}DHARMA%F{220} Initiative Plugin Manager (%F{33}zdharma/zinit%F{220})…%f"
+    echo "Installing zinit"
     command mkdir -p "${HOME}/.zinit" && command chmod g-rwX "${HOME}/.zinit"
     # shellcheck disable=SC2015
-    command git clone 'https://github.com/zdharma/zinit' "${HOME}/.zinit/bin" &&
-        print -P "%F{33}▓▒░ %F{34}Installation successful.%f%b" ||
-        print -P "%F{160}▓▒░ The clone has failed.%f%b"
+    command git clone 'https://github.com/zdharma/zinit' "${HOME}/.zinit/bin"
 fi
