08d43654881d66be8eaff7936996c5290d5cf8fb
Rafhaan Shah <RafhaanShah@users.noreply.github.com>
2021-03-03 17:36:40 +0000

    Add Termux configs

---
+++ setup/termux.sh
@@ -0,0 +1,49 @@
+#!/data/data/com.termux/files/usr/bin/env bash
+# shellcheck shell=bash
+
+# exit on any error or failed piped commands
+set -eo pipefail
+
+# dotfile repo folder
+DOTFILE_DIR="${HOME}/Dotfiles"
+# shellcheck source=../shell/helpers.sh
+source "${DOTFILE_DIR}/shell/helpers.sh"
+
+if ! _is_termux; then
+    echo "This is only for Termux"
+    exit
+fi
+
+mkdir -p "${HOME}/.termux"
+cp "${DOTFILE_DIR}/config/termux/termux.properties" "${HOME}/.termux"
+cp "${DOTFILE_DIR}/config/termux/colors.properties" "${HOME}/.termux"
+# cp "${DOTFILE_DIR}/config/font/font.tff" "${HOME}/.termux"
+
+# install termux packages
+pkg upgrade
+xargs <"${DOTFILE_DIR}/packages/termux.txt" pkg install -y
+pkg clean
+
+# https://wiki.termux.com/wiki/Termux-setup-storage
+termux-setup-storage
+
+# setup nodejs
+mkdir -p "${HOME}/.npm-global/bin"
+npm config set prefix "${HOME}/.npm-global"
+_add_to_path "${HOME}/.npm-global/bin"
+
+npm install "fx"
+
+# zsh setup
+# shellcheck source=zsh.sh
+source "${DOTFILE_DIR}/setup/zsh.sh"
+
+# set shell
+echo "Changing shell..."
+chsh -s "$(command -v zsh)"
+
+# intial ssh setup instructions:
+# pkg install openssh
+# passwd
+# sshd
+# ssh -p 8022 TERMUX_IP
---
+++ shell/termux.sh
@@ -0,0 +1,10 @@
+### termux specific configs
+# shellcheck shell=bash
+
+alias ssh-on='sshd'
+alias ssh-off='pkil sshd'
+
+alias open='termux-open'
+
+alias copy='termux-clipboard-set'
+alias paste='termux-clipboard-get'
