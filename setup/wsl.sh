9b47c66546091c9ba6ecf5ef10d095b48ed4e453
Rafhaan Shah <RafhaanShah@users.noreply.github.com>
2022-01-14 23:27:29 +0000

    Add WSL setup

---
+++ setup/wsl.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+# exit on any error or failed piped commands
+set -eo pipefail
+
+# dotfile repo folder
+DOTFILE_DIR="${HOME}/Dotfiles"
+[ ! -d "${DOTFILE_DIR}" ] && {
+    echo "Invalid Dotfile folder"
+    exit 1
+}
+# shellcheck source=../shell/helpers.sh
+source "${DOTFILE_DIR}/shell/helpers.sh"
+
+echo "Running WSL setup script..."
+
+if ! _is_wsl; then
+    echo "This is only for WSL"
+    exit
+fi
+
+# https://github.com/nullpo-head/WSL-Hello-sudo
+setup_wsl_hello_sudo() {
+    echo "Running WSL Hello sudo setup..."
+    curl -sSL "http://github.com/nullpo-head/WSL-Hello-sudo/releases/latest/download/release.tar.gz" -o "release.tar.gz"
+    tar xvf "release.tar.gz"
+    (cd "release" && "./install.sh")
+    rm -rf "release" "release.tar.gz"
+}
+
+setup_wsl_hello_sudo
+
+echo "Done settings up WSL"
