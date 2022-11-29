61f1ae0e9685a979dfff2f45c1572542e9593901
Rafhaan Shah <RafhaanShah@users.noreply.github.com>
2022-11-29 10:41:14 +0000

    Update macos.sh

--- setup/macos.sh
+++ setup/macos.sh
@@ -43,6 +43,7 @@ source "${DOTFILE_DIR}/setup/zsh.sh"
 
 # iterm2 config
 defaults write com.googlecode.iterm2 PrefsCustomFolder "${DOTFILE_DIR}/config/iTerm2"
+defaults write com.googlecode.iterm2 LoadPrefsFromCustomFolder -bool true
 
 # TextMate config
 ln -s "${DOTFILE_DIR}/config/TextMate/.tm_properties" "${HOME}/.tm_properties"
